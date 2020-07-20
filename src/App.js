13f8a954dd4774b70c4d2f1bc60f4ac229a4482c
arti upadhyay <artiupadhyay28@gmail.com>
2020-07-20 00:25:26 -0600

    Login screen created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Login from './components/login';
+
+function App() {
+    return (<div>
+         
+         <Login name="Arti"/>
+        
+        </div>
+    );
+}
+
+export default App;
