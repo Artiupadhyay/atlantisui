7dff75268478a6701da52ea52d827ccb06c62418
Amit Kuntal <amit.kuntal@loanframe.com>
2020-09-06 21:53:38 +0530

    backend url added

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,5 +1,5 @@
 var config = {
-    baseurl: "http://127.0.0.1:8000/"//"http://ec2-15-206-173-26.ap-south-1.compute.amazonaws.com/"//"https://atlantisbackend.herokuapp.com/"
+    baseurl: "https://atlantisbackend.herokuapp.com/"//"http://127.0.0.1:8000/"//"http://ec2-15-206-173-26.ap-south-1.compute.amazonaws.com/"
 }
 
 export default config;
